5540d53de670911088b94e87f5ace02e965c31b5
Jordi <romero.suarez.jordi@gmail.com>
2022-02-27 20:11:12 +0100

    New textlines

--- src/locale/en.js
+++ src/locale/en.js
@@ -9,3 +9,13 @@ export const LabelsDrawer = {
     LIBRARY:"My library",
     FORUMS:"Forums",
 }
+
+export const LabelsMain ={
+    LOREM: "Lorem ipsum",
+    DOLOR: "Dolor sit amet",
+    DISCOVERY: "Start the discovery",
+    SORT: 'Sort by',
+    POPULAR:'Popular',
+    RELEASE: 'Release date',
+    RATING: 'Rating',
+}
