eddfec162649b7cb7627996f9bbc5f2ca3104a4b
Jordi <romero.suarez.jordi@gmail.com>
2022-02-27 12:17:41 +0100

    Added icons

--- src/resources/Icons.js
+++ src/resources/Icons.js
@@ -1,21 +1,11 @@
-import PublicIcon from '@material-ui/icons/Public';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import VerifiedUserOutlinedIcon from '@material-ui/icons/VerifiedUserOutlined';
-import IndeterminateCheckBoxRoundedIcon from '@material-ui/icons/IndeterminateCheckBoxRounded';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import HomeIcon from '@material-ui/icons/Home';
+import StarIcon from '@material-ui/icons/Star';
 
 const Icons = {
     'HOME': HomeIcon,
-    'PUBLIC': PublicIcon,
+    'STAR': StarIcon,
     'MORE': MoreHorizIcon,
-    'RESPONSES': IndeterminateCheckBoxRoundedIcon,
-    'PRIVATE': LockOutlinedIcon,
-    'VERIFIED': VerifiedUserOutlinedIcon,
-    'INFO': InfoOutlinedIcon,
-    'ARROW_DOWN': KeyboardArrowDownIcon,
 }
 
 export default Icons
