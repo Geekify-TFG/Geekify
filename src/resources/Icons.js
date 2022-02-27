75c3fe8c2f7e71c6e75822c41c3585fba9aee2bb
Jordi <romero.suarez.jordi@gmail.com>
2022-02-27 12:39:49 +0100

    Added bookmark, check paddings

--- src/resources/Icons.js
+++ src/resources/Icons.js
@@ -1,11 +1,12 @@
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import HomeIcon from '@material-ui/icons/Home';
 import StarIcon from '@material-ui/icons/Star';
-
+import BookmarkIcon from '@material-ui/icons/Bookmark';
 const Icons = {
     'HOME': HomeIcon,
     'STAR': StarIcon,
     'MORE': MoreHorizIcon,
+    'BOOKMARK':BookmarkIcon,
 }
 
 export default Icons
