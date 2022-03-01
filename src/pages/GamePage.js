04d633438da9bbb0c1335c2ccf6c26b111453044
Jordi <romero.suarez.jordi@gmail.com>
2022-03-01 20:43:31 +0100

    Added comments and info card

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,17 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {Avatar, ButtonGroup, Card, CardMedia, Grid} from "@material-ui/core";
+import {
+    Avatar,
+    ButtonGroup,
+    Card,
+    CardMedia,
+    Grid, IconButton,
+    InputAdornment,
+    List,
+    ListItem,
+    ListItemText,
+    TextField
+} from "@material-ui/core";
 import {Button, Typography} from "@mui/material";
 import {BASE_PATH, GAME} from "../resources/ApiUrls";
 import axios from "axios";
 import {useHistory, useLocation} from "react-router-dom";
 import SearchBar from "../components/SearchBar/SearchBar";
 import {AppColors} from "../resources/AppColors";
-import {LabelsMain} from "../locale/en";
+import {LabelsGamePage, LabelsMain} from "../locale/en";
 import {makeStyles} from "@mui/styles";
 import {AppTextsFontSize, AppTextsFontWeight} from "../resources/AppTexts";
+import CardGeekify from "../components/CardGeekify/CardGeekify";
+import accountIcon from "../img/account_icon.svg"
+import CardComment from "../components/CardComment/CardComment";
 
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         minWidth: 275,
     },
@@ -38,16 +51,34 @@ const useStyles = makeStyles({
     },
     cardHeaderContent: {
         overflow: "hidden"
+    },
+    textFieldLabel: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: AppColors.PRIMARY,
+                opacity: '0.2',
+                borderRadius: 10,
+            },
+        }, '& .MuiInputBase-root': {
+            color: AppColors.PRIMARY,
+        }, '& .MuiInputLabel-root': {
+            color: AppColors.PRIMARY,
+        }, '& .MuiTextField-root': {
+            height: '25em',
+        },
+        color: AppColors.PRIMARY,
+        backgroundColor: AppColors.BACKGROUND_DRAWER,
+        borderRadius: 10,
     }
 
 
-});
+}));
 
 const GamePage = () => {
     const [game, setGame] = useState();
     const location = useLocation();
     const classes = useStyles();
-
+    const [comment,setComment] = useState()
     const history = useHistory()
     const idGame = location.state.detail
 
@@ -73,7 +104,7 @@ const GamePage = () => {
                     backgroundSize: "cover",
 
                 }}>
-                    <Grid container direction={"row"} justifyContent={"space-between"}  spacing={20}>
+                    <Grid container direction={"row"} justifyContent={"space-between"} spacing={20}>
                         <Grid item style={{margin: '2em'}}>
                             <SearchBar/>
                         </Grid>
@@ -94,7 +125,7 @@ const GamePage = () => {
                             </Button>
                         </Grid>
                     </Grid>
-                    <Grid container direction={"row"}>
+                    <Grid container direction={"row"} style={{flexWrap: "nowrap"}}>
 
                         <Grid item style={{margin: '4em'}}>
                             <Card style={{height: '400px', width: '300px', position: "relative", borderRadius: 20}}
@@ -114,45 +145,153 @@ const GamePage = () => {
 
                             </Card>
                         </Grid>
-                        <Grid item style={{margin: '4em', marginLeft: 0}}>
-                            <Button style={{backgroundColor: AppColors.BACKGROUND, borderRadius: 20}} disabled={true}>
-                                <Typography style={{color: AppColors.WHITE, marginBottom: 0}} gutterBottom variant="h5"
-                                            component="h2">
-                                    {game.genres[0].name}
-                                </Typography>
-                            </Button>
-                            <Typography
-                                style={{
-                                    fontSize: '40px',
-                                    color: AppColors.WHITE
-                                }}>{game.name.toUpperCase()}</Typography>
-                            <ButtonGroup style={{width: '500px'}} color="primary"
-                                         aria-label="outlined primary button group">
-                                {game.tags.slice(0,3).map((type) => (
-                                    <Button style={{backgroundColor: AppColors.WHITE, borderRadius: 20,}} disabled={true}>
-                                        <Typography style={{color: AppColors.BLACK, marginBottom: 0}} gutterBottom variant="h8"
-                                                    >
-                                            {type.name}
-                                        </Typography>
-                                    </Button>
-                                ))}
-                            </ButtonGroup>
+                        <Grid container direction={"column"} item style={{margin: '4em', marginLeft: 0}}>
+                            <Grid item style={{marginBottom: '1em'}}>
+                                <Button
+                                    style={{backgroundColor: AppColors.BACKGROUND, borderRadius: 20, maxWidth: '10em'}}
+                                    disabled={true}>
+                                    <Typography style={{color: AppColors.WHITE, marginBottom: 0}} gutterBottom
+                                                variant="h5"
+                                                component="h2">
+                                        {game.genres[0].name}
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Grid item style={{marginBottom: '1em'}}>
 
+                                    <Typography
+                                        style={{
+                                            fontSize: '40px',
+                                            color: AppColors.WHITE
+                                        }}>{game.name.toUpperCase()}</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item style={{marginBottom: '1em'}}>
+
+                                <ButtonGroup style={{width: '500px'}} color="primary"
+                                             aria-label="outlined primary button group">
+                                    {game.tags.slice(0, 3).map((type) => (
+                                        <Button style={{backgroundColor: AppColors.WHITE, borderRadius: 20,}}
+                                                disabled={true}>
+                                            <Typography style={{color: AppColors.BLACK, marginBottom: 0}} gutterBottom
+                                                        variant="h8"
+                                            >
+                                                {type.name}
+                                            </Typography>
+                                        </Button>
+                                    ))}
+                                </ButtonGroup>
+                            </Grid>
+                            <Grid item style={{marginBottom: '1em'}}>
+
+                                <Typography
+                                    style={{
+                                        fontSize: '20px',
+                                        color: AppColors.WHITE
+                                    }}>{LabelsGamePage.DESCRIPTION}</Typography>
+                            </Grid>
+
+                            <Grid item style={{marginBottom: '1em'}}>
+
+                                <Typography
+                                    style={{
+                                        color: AppColors.WHITE
+                                    }}>{game.description}</Typography>
+
+                            </Grid>
                         </Grid>
                     </Grid>
 
                 </Grid>
                 <Grid container
-                      direction={"column"}>
+                      direction={"row"} style={{marginTop: '2em', marginBottom: '2em'}}>
                     <Grid item style={{marginLeft: '4em'}}>
-                        <Typography
-                            style={{fontSize: '40px', color: AppColors.WHITE}}>{LabelsMain.DISCOVERY}</Typography>
+                        <CardGeekify bg={AppColors.BACKGROUND_CARD} height={'274px'} width={'349px'}>
+                            <Grid
+                                container
+                            >
+                                <List style={{marginLeft:'1em',marginTop:'0.5em'}}>
+                                    <ListItem>
+                                        <ListItemText style={{color:AppColors.WHITE,marginRight:'5em'}}
+                                            primary={LabelsGamePage.RELEASE}
+                                        />
+                                        <ListItemText style={{color:AppColors.SECONDARY}}
+                                            primary={game.released}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText style={{color:AppColors.WHITE,marginRight:'9em'}}
+                                            primary={LabelsGamePage.PLATFORM}
+                                        />
+                                        <ListItemText style={{color:AppColors.SECONDARY}}
+                                            primary={game.playtime}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText style={{color:AppColors.WHITE,marginRight:'5em'}}
+                                            primary={LabelsGamePage.DURATION}
+                                        />
+                                        <ListItemText style={{color:AppColors.SECONDARY}}
+                                            primary={game.playtime}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText style={{color:AppColors.WHITE,marginRight:'5em'}}
+                                            primary={LabelsGamePage.DEVELOPER}
+                                        />
+                                        <ListItemText style={{color:AppColors.SECONDARY}}
+                                            primary={game.developers[0].name}
+                                        />
+                                    </ListItem>
+
+                                    <ListItem>
+                                        <ListItemText style={{color:AppColors.WHITE,marginRight:'5em'}}
+                                            primary={LabelsGamePage.PUBLISHER}
+                                        />
+                                        <ListItemText style={{color:AppColors.SECONDARY}}
+                                            primary={game.publishers[0].name}
+                                        />
+                                    </ListItem>
+                                </List>
+
+
+                            </Grid>
+
+                        </CardGeekify>
+
                     </Grid>
-                    <Grid container>
-                        <Grid item style={{marginBottom: '4em', marginLeft: '4em'}}>
+                    <Grid item style={{marginLeft: '4em'}}>
+                        <Grid item style={{marginBottom: '4em', }}>
                             <Typography
-                                style={{fontSize: '20px', color: AppColors.SUBTEXT}}>{LabelsMain.SORT}</Typography>
+                                style={{
+                                    fontSize: '20px',
+                                    color: AppColors.WHITE
+                                }}>{LabelsGamePage.COMMUNITY}</Typography>
+                            <TextField
+                                style={{width:'400px'}}
+                                onChange={(e) => setComment(e.target.value)}
+                                type="text"
+                                label={`Publish about ${game.name}`}
+                                margin="normal"
+                                variant="outlined"
+                                multiline
+                                className={classes.textFieldLabel}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+
+                                                <img alt='icon'
+                                                     src={accountIcon}/>
+                                        </InputAdornment>
+
+                                    ),
+                                }}
+                            />
+                            <CardComment width={'400px'} time={"2 minutes ago"} title={"Hola"} comment={LabelsGamePage.COMMENT_EXAMPLE} bg={AppColors.BACKGROUND_DRAWER} />
                         </Grid>
+
+
 
 
                     </Grid>
