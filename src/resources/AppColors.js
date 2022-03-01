66df9a65b9f419050e0dd6182b9b5440336ea500
Jordi <romero.suarez.jordi@gmail.com>
2022-03-01 20:43:36 +0100

    New colors

--- src/resources/AppColors.js
+++ src/resources/AppColors.js
@@ -1,8 +1,9 @@
 export const AppColors = {
     'PRIMARY': '#6563FF',
-    'SECONDARY': '#7F8DAA',
+    'SECONDARY': '#2C2B71',
     'BACKGROUND': '#1D1D1D',
     'BACKGROUND_DRAWER': '#37393B',
+    'BACKGROUND_CARD': '#4B4AC0',
     'BLACK': '#000000',
     'WHITE': '#FFFFFF',
     'RED': '#F0142F',
