7aaed474042cdf4dfa4f2ff547f18af29433dd40
Jordi <romero.suarez.jordi@gmail.com>
2022-03-03 20:15:45 +0100

    Mocks for game and search pages

---
+++ src/mocks/SearchMocks.js
@@ -0,0 +1,82 @@
+import {LabelsSearchPage} from "../locale/en";
+
+export const platformsMock = [
+    {
+        label: LabelsSearchPage.PLAYSTATION,
+        value: "PLAYSTATION"
+    },
+    {
+        label: LabelsSearchPage.NINTENDO,
+        value: "NINTENDO"
+    },
+    {
+        label: LabelsSearchPage.XBOX,
+        value: "XBOX"
+    },
+    {
+        label: LabelsSearchPage.PC,
+        value: "PC"
+    },
+    {
+        label: LabelsSearchPage.OTHERS,
+        value: "Others"
+    },
+]
+
+export const genresMock = [
+    {
+        label: LabelsSearchPage.ACTION,
+        value: "Action"
+    },
+    {
+        label: LabelsSearchPage.ADVENTURE,
+        value: "NINTENDO"
+    },
+    {
+        label: LabelsSearchPage.SHOOTER,
+        value: "Shooter"
+    },
+    {
+        label: LabelsSearchPage.PUZZLE,
+        value: "Puzzle"
+    },
+    {
+        label: LabelsSearchPage.RPG,
+        value: "Rpg"
+    },
+    {
+        label: LabelsSearchPage.INDIE,
+        value: "Indie"
+    },
+    {
+        label: LabelsSearchPage.STRATEGY,
+        value: "Strategy"
+    },
+    {
+        label: LabelsSearchPage.FAMILY,
+        value: "Family"
+    },
+    {
+        label: LabelsSearchPage.SPORTS,
+        value: "Sports"
+    },
+    {
+        label: LabelsSearchPage.FIGHTING,
+        value: "Fighting"
+    },
+]
+
+export const numPlayersMock = [
+    {
+        label: LabelsSearchPage.ONLY_ME,
+        value: "Only me"
+    },
+    {
+        label: LabelsSearchPage.COOP,
+        value: "Cooperative"
+    },
+    {
+        label: LabelsSearchPage.MULTIPLAYER,
+        value: "Multiplayer"
+    },
+]
