e16d595e6c4c1c5513af2a6a65c7ef4e2fdf75dc
Jordi <romero.suarez.jordi@gmail.com>
2022-03-08 18:41:26 +0100

    Forums mock

---
+++ src/mocks/ForumPostsMock.js
@@ -0,0 +1,44 @@
+export const forumPostsMock = [
+    {
+        id: 1,
+        username: "Pedro",
+        timeAgo: "2 minutes ago",
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 2,
+        username: "Jose",
+        timeAgo: '3 days ago',
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  aasdadasdadsLorem Ipsum is simply dummy text of the printing and typesetting  Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 3,
+        username: "Manuel",
+        timeAgo: '3 days ago',
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 4,
+        username: "Pau",
+        timeAgo: '5 days ago',
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  aasdadasdadsLorem Ipsum is simply dummy text of the printing and typesetting  Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 5,
+        username: "Juan",
+        timeAgo: '5 days ago',
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 6,
+        username: "Alberto",
+        timeAgo: '5 days ago',
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  aasdadasdadsLorem Ipsum is simply dummy text of the printing and typesetting  Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+    {
+        id: 7,
+        username: "Games for girlfriend",
+        timeAgo: 2,
+        comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting  '
+    },
+]
