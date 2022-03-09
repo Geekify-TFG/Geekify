49e2211032fcba3f59bd97f8febc6634643473e3
Jordi <romero.suarez.jordi@gmail.com>
2022-03-09 19:56:50 +0100

    New mock

---
+++ src/mocks/FollowingUsersMock.js
@@ -0,0 +1,44 @@
+export const followingUsersMock = [
+    {
+        id: 1,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 2,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 3,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 4,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 5,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 6,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+    {
+        id: 7,
+        username: "Pedro",
+        gamesCommon: "3 gammes in comon",
+        avatar: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
+    },
+]
