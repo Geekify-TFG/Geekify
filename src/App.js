124f145669b6324764186f57cb9fb6f341549ebc
Jordi <romero.suarez.jordi@gmail.com>
2022-03-10 19:29:08 +0100

    Route calendar

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import CollectionPage from "./pages/CollectionPage";
 import ForumsPage from "./pages/ForumsPage";
 import ForumPage from "./pages/ForumPage";
 import ProfilePage from "./pages/ProfilePage";
+import CalendarPage from "./pages/CalendarPage";
 
 
 const drawerWidth = 240;
@@ -189,6 +190,7 @@ function App() {
                 <RouteMain exact path="/" component={MainPage}/>
                 <RouteMain path={"/game/:id"} component={() => <GamePage/>}/>
                 <RouteMain path={"/profile"} component={() => <ProfilePage/>}/>
+                <RouteMain path={"/calendar"} component={() => <CalendarPage/>}/>
                 <RouteMain path={"/search"} component={() => <SearchPage/>}/>
                 <RouteMain path={"/collections"} component={() => <CollectionsPage/>}/>
                 <RouteMain path={"/collection/:id"} component={() => <CollectionPage/>}/>
