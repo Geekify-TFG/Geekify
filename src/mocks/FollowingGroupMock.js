8843afaacad56a6e11c9e6c057a6810a879628cf
Jordi <romero.suarez.jordi@gmail.com>
2022-03-14 20:04:55 +0100

    Changed texts

--- src/mocks/FollowingGroupMock.js
+++ src/mocks/FollowingGroupMock.js
@@ -1,43 +1,43 @@
 export const followingGroupMock = [
     {
         id: 1,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 2,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 3,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 4,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 5,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 6,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
     {
         id: 7,
-        groupName: "Games for girlfriend",
+        groupName: "Games for chilling",
         numParticipants: 2,
         image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg'
     },
