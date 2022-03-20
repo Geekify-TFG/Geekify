7a714dc437f9e8fd2dae8662bac54d86671c9797
Jordi <romero.suarez.jordi@gmail.com>
2022-03-20 12:29:45 +0100

    API games, search, order and detail WIP

--- api/app.py
+++ api/app.py
@@ -14,6 +14,7 @@ from api.models.accountModel import AccountModel
 # resources imports
 from resources.account import Accounts
 from resources.login import LogIn
+from resources.games import Games, GamesByTitle, GamesByOrder,GameDetail
 
 app = Flask(__name__)
 environment = config['development']
@@ -30,75 +31,6 @@ AccountModel.collection = db.get_database.accounts
 CONNECTION_STRNG = "mongodb+srv://jromero:050899@geekify.q6113.mongodb.net/test?retryWrites=true&w=majority"
 mongo = pymongo.MongoClient(CONNECTION_STRNG, tls=True, tlsAllowInvalidCertificates=True)
 
-api_rawg = "https://api.rawg.io/api/games?key=40f3cb2ff2c94a5889d3d6c865415ec5"
-response = requests.get(api_rawg)
-
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    # Recieving data
-    email = request.json['email']
-    username = request.json['username']
-    password = request.json['password']
-
-    if username and email and password:
-        hashed_password = generate_password_hash(password)
-        id = mongo.test.users.insert_one({
-            'username': username,
-            'email': email,
-            'password': hashed_password,
-
-        })
-        response = {
-            'id': str(id),
-            'username': username,
-            'email': email,
-            'password': password,
-        }
-        return response
-    else:
-        return not_found()
-    return {'message': 'recieved'}
-
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = mongo.test.users.find()
-    response = json_util.dumps(users)
-    return Response(response, mimetype='application/json')
-
-
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-    user = mongo.test.users.find_one({'_id': ObjectId(id)})
-    response = json_util.dumps(user)
-    return Response(response, mimetype='application/json')
-
-
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = mongo.test.users.delete_one({'_id': ObjectId(id)})
-    response = jsonify({'message': 'User ' + id + 'was deleted'})
-    return response
-
-
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    email = request.json['email']
-    username = request.json['username']
-    password = request.json['password']
-    if username and email and password:
-        hashed_password = generate_password_hash(password)
-        mongo.test.users.update_one({'_id': ObjectId(id)}, {'$set': {
-            'username': username,
-            'email': email,
-            'password': hashed_password,
-
-        }})
-        response = jsonify({'message': 'User ' + id + 'was updated succesfully'})
-
-        return response
-
 
 @app.errorhandler(404)
 def not_found(error=None):
@@ -110,14 +42,12 @@ def not_found(error=None):
     return message
 
 
-@app.route('/info', methods=['GET'])
-def get_info():
-    # Recieving data de la API de rawg
-    return response.json()
-
-
 api.add_resource(Accounts, '/account/email/<string:email>', '/account/id/<string:id>', '/account/user')
 api.add_resource(LogIn, '/login')
 
+api.add_resource(Games, '/games')
+api.add_resource(GameDetail, '/game/<string:id>')
+api.add_resource(GamesByTitle, '/games/title/<string:title>')
+api.add_resource(GamesByOrder, '/games/filter/<string:order>')
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
