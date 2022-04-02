29ad268189ec251ece17b955c30e39fdbb607b91
Jordi <romero.suarez.jordi@gmail.com>
2022-04-02 19:21:09 +0200

    Added apiUrls

--- src/resources/ApiUrls.js
+++ src/resources/ApiUrls.js
@@ -1,7 +1,7 @@
 // Base
 const _DOMAIN = "https://api.rawg.io/api"; // Domain of the server
-//const _MY_DOMAIN = "http://127.0.0.1:5000"; // Domain of the server
-const _MY_DOMAIN = "https://geekify-be.herokuapp.com/"; // Domain of the server
+const _MY_DOMAIN = "http://127.0.0.1:5000"; // Domain of the server
+//const _MY_DOMAIN = "https://geekify-be.herokuapp.com/"; // Domain of the server
 
 const _API_KEY = "key=40f3cb2ff2c94a5889d3d6c865415ec5\n"; // Path to the API
 export const BASE_PATH = `${_DOMAIN}`; // Base URL of the backend
@@ -29,18 +29,30 @@ export const GAME_IMAGES = (id) => `${_GAME_PATH}${id}${_SCREENSHOTS_PATH}?${_AP
 
 
 
+//Login/Register paths
 const _LOGIN_PATH = "/login";
 const _REGISTER_PATH = "/account/user";
+
+export const LOGIN_URL = `${MY_BASE_PATH}${_LOGIN_PATH}`
+export const REGISTER_URL = `${MY_BASE_PATH}${_REGISTER_PATH}`
+
+//Games paths
 
 
 const _MY_GAMES_PATH = "/games";
 const _MY_GAME_PATH = "/game";
 const _MY_TITLE_PATH = "/title";
 const _FILTER_PATH = "/filter";
-
-export const LOGIN_URL = `${MY_BASE_PATH}${_LOGIN_PATH}`
-export const REGISTER_URL = `${MY_BASE_PATH}${_REGISTER_PATH}`
 export const MY_GAMES = `${_MY_GAMES_PATH}`
 export const MY_GAME = (id) => `${_MY_GAME_PATH}${id}`
 export const MY_GAME_SEARCH = (title) => `${_MY_GAMES_PATH}${_MY_TITLE_PATH}/${title}`
 export const MY_GAMES_FILTER = (filter) => `${_MY_GAMES_PATH}${_FILTER_PATH}/${filter}`
+
+
+//Collections paths
+const _COLLECTIONS_PATH = "/collections";
+const _COLLECTION_PATH = "/collection";
+const _COLLECTION_GAME_PATH = "/collectionGame";
+export const MY_COLLECTIONS= `${_COLLECTIONS_PATH}`
+export const COLLECTION_GAME= (id) =>`${_COLLECTION_GAME_PATH}/${id}`
+export const MY_COLLECTION = (id) => `${_COLLECTION_PATH}/${id}`
