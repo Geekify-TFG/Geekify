db3f414af34bf03a4c9f3a155902578933821edc
Jordi <romero.suarez.jordi@gmail.com>
2022-05-09 18:38:20 +0200

    More wait to calendarTest

--- cypress/integration/calendar.spec.js
+++ cypress/integration/calendar.spec.js
@@ -13,9 +13,9 @@ describe('Calendar tests', () => {
         cy.get('button').contains("Log in").should('be.visible').click()
         cy.wait(3000)
         cy.get('[id="Release Calendar"]').should('be.visible').click()
-        cy.wait(5000)
+        cy.wait(6000)
         cy.get('[data-testid="ButtonKey"]').contains("My calendar").click()
-        cy.wait(5000)
+        cy.wait(6000)
         cy.get('[data-testid="calendarCard"]').contains("Diablo: Immortal").click()
     })
 
